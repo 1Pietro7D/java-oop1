e39e53b0b729b193b26b9af4ccc4bbea96f97115
PietroDanieli <1pie7d@gmail.com>
2023-01-27 16:19:19 +0100

    implement some function and test class in his debugger

--- src/org/lesson/java/shop/Prodotto.java
+++ src/org/lesson/java/shop/Prodotto.java
@@ -1,37 +1,44 @@
 package org.lesson.java.shop;
 
+import java.util.Random;
+
 public class Prodotto {
 	int cod; // max 8 cifre
-	String name;
-	String description;
+	String name = "asciugamario";
+	String description = "può asciugare mario";
 	double price;
 	int iva = 22; // 22
-	
-	
-	
-	/** Constructor all variable
-	 * @param cod
+
+	/**
+	 * Constructor all variable
+	 * 
 	 * @param name
 	 * @param description
 	 * @param price
 	 * @param iva
 	 */
-	public Prodotto(int cod, String name, String description, double price, int iva) {
-		this.cod = cod;
-		this.name = name;
-		this.description = description;
+	public Prodotto(double price) {
+		Random rand = new Random();
+		this.cod = rand.nextInt(100000000);
 		this.price = price;
-		this.iva = iva;
 	}
-	
 
+	double postIva(double price) {
+		double priceMoreIva = (100 + this.iva) * price / 100;
+		return priceMoreIva;
+	};
 
+	String generateFullName() {
+		String fullName = Integer.toString(cod) + "-" + name;
+		return fullName;
+	};
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Prodotto productTest=new Prodotto(111,"asciugamario","può asciugare mario",10.5,22);
-		
-		System.out.println(productTest.name);
+		Prodotto productTest = new Prodotto(10.0);
+		System.out.print("prezzo base " + String.format("%.2f", productTest.price) + "€ --");
+		System.out.println(" prezzo con iva " + String.format("%.2f", productTest.postIva(productTest.price)) + "€");
+		System.out.println(productTest.generateFullName());
 	}
 
 }
