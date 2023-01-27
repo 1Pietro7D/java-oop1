b7d7951326e8157d113d8b9932891f07cec7dafc
PietroDanieli <1pie7d@gmail.com>
2023-01-27 15:09:22 +0100

    correct position of variables

--- src/org/lesson/java/shop/Prodotto.java
+++ src/org/lesson/java/shop/Prodotto.java
@@ -1,14 +1,14 @@
 package org.lesson.java.shop;
 
 public class Prodotto {
-
+	int cod; // max 8 cifre
+	String name;
+	String description;
+	double price;
+	int iva = 22; // 22
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int cod; // max 8 cifre
-		String name;
-		String description;
-		double price;
-		int iva = 22; // 22
+		
 	}
 
 }
