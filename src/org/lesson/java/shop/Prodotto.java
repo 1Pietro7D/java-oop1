13dac0e44aae6e4772aab197ed6453595f800587
PietroDanieli <1pie7d@gmail.com>
2023-01-27 15:23:18 +0100

    first test

--- src/org/lesson/java/shop/Prodotto.java
+++ src/org/lesson/java/shop/Prodotto.java
@@ -6,9 +6,32 @@ public class Prodotto {
 	String description;
 	double price;
 	int iva = 22; // 22
+	
+	
+	
+	/** Constructor all variable
+	 * @param cod
+	 * @param name
+	 * @param description
+	 * @param price
+	 * @param iva
+	 */
+	public Prodotto(int cod, String name, String description, double price, int iva) {
+		this.cod = cod;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.iva = iva;
+	}
+	
+
+
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Prodotto productTest=new Prodotto(111,"asciugamario","può asciugare mario",10.5,22);
 		
+		System.out.println(productTest.name);
 	}
 
 }
